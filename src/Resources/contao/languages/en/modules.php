56d86e8e9f85ac50dbdd58e68453915f4540cfa8
Arne Borchert <arne.borchert@fipps.de>
2018-01-22 17:25:26 +0100

    dca und languages importiert

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * rssImport
+ *
+ * Language file for modules (en)
+ *
+ * Copyright (c) 2011, 2014 agentur fipps e.K
+ *
+ * @copyright 2011, 2014 agentur fipps e.K.
+ * @author Arne Borchert
+ * @package fipps\rssImport
+ * @license LGPL
+ */
+
+// Back end modules
+$GLOBALS['TL_LANG']['MOD']['rssImport'] = array(
+        'RSS Import',
+        'RSS/Atom importfunctions'
+);
